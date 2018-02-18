4ef71913d9650a41f65049b1538417f6d830e86a
Matthew Herron <matthew.herron77@gmail.com>
2018-02-17 22:51:09 -0600

    Fixed anomoly prices and added volume size to the candlesticks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+## [1.2.0] - 2018-02-17
+### Added
+- Volume sizes for each candlestick
+
+### Fixed
+- Inconsistencies in the high and low values.  Eliminated anomoly values that skewed candle spreads.
+
 ## [1.0.1] - 2018-01-30
 ### Added
 - Added close, height and spread values to the open candlesticks
---
+++ src/Exchange.js
@@ -0,0 +1,31 @@
+const Gdax = require('gdax');
+
+class Exchange {
+  constructor(product) {
+    this.product = product;
+
+    this.websocket = new Gdax.WebsocketClient([this.product]);
+    this._initSocket();
+
+    let orderbookSync = new Gdax.OrderbookSync([this.product]);
+    this.orderbook = orderbookSync.books[this.product];
+  }
+
+  _initSocket() {
+    this.websocket.on('open', () => {
+      this.websocket.subscribe({ product_ids: [this.product], channels: ['ticker'] });
+    });
+    this.websocket.on('error', (err) => {
+      console.log(err);
+    });
+  }
+}
+
+let instance;
+
+module.exports.getInstance = (product) => {
+  if (!instance) {
+    instance = new Exchange(product);
+  }
+  return instance;
+};
