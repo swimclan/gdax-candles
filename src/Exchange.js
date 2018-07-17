26f5bfae04121f32a14f6d93592d8915e98e11cc
Flippo24 <Flippo24@users.noreply.github.com>
2018-07-17 21:52:54 +0200

    reducing bandwidth
    We are interested in 'match' messages, so we don't need the 'full' channel.

--- src/Exchange.js
+++ src/Exchange.js
@@ -4,20 +4,16 @@ class Exchange {
   constructor(product) {
     this.product = product;
 
-    this.websocket = new Gdax.WebsocketClient([this.product]);
     this.websocket = new Gdax.WebsocketClient(
       [this.product],
       'wss://ws-feed.pro.coinbase.com',
       null,
-      { channels: ['full'] }
+      { channels: ['matches'] }
     );
     this._initSocket();
   }
 
   _initSocket() {
-    this.websocket.on('open', () => {
-      this.websocket.subscribe({ product_ids: [this.product], channels: ['ticker'] });
-    });
     this.websocket.on('error', (err) => {
       console.log(err);
     });
